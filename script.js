402eea6d92eb79b6e7941ac304901e994e5e4e15
PieStw <piero.stalteri@icloud.com>
2024-11-11 14:45:43 +0100

    aggiunti metodi

--- script.js
+++ script.js
@@ -3,5 +3,20 @@ class Veicolo {
         this.marca = marca;
         this.anno = anno;
         this.colore = colore;
-      }
+    }
+
+    calcoloEta(){
+        const date = new Date;
+        console.log(`Questa vettura ha i seguenti anni: ${date.getFullYear() - this.anno}`);
+    }
+
+    informazioni(){
+        console.log(`Questa vettura è di marca: ${this.marca}\nQuesta vettura è dell'anno: ${this.anno}\nQuesta vettura è di colore: ${this.colore}`)
+    }
+
   }
+
+
+const macchina = new Veicolo("Fiat", 2019, "Blu");
+macchina.informazioni();
+macchina.calcoloEta();
